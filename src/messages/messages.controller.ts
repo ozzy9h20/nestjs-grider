e169fd180ef2fc4b72761f993c4cd1bdf7126c5a
M. Endyarroziqin <endy@millennia-solusi.id>
2023-11-25 10:25:15 +0700

    Section 05 - Chapter 27 ~ Reporting Errors with Exceptions

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common'
 import { CreateMessageDto } from './dtos/create-message.dto'
 import { MessagesService } from './messages.service'
 
@@ -23,7 +30,13 @@ export class MessagesController {
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    return this.messageService.findOne(id)
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messageService.findOne(id)
+
+    if (!message) {
+      throw new NotFoundException('message not found')
+    }
+
+    return message
   }
 }
