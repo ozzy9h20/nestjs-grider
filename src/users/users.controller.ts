2c5efee5c85aa08868337c9fe1533db89bffea61
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2023-11-25 23:23:26 +0700

    Section 09 - Chapter 54-57 ~ Users Service CRUD

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common'
 import { UsersService } from './users.service'
 import { CreateUserDto } from './dtos/create-user.dto'
+import { UpdateUserDto } from './dtos/update-user.dto'
 
 @Controller('auth')
 export class UsersController {
@@ -9,5 +19,25 @@ export class UsersController {
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     this.usersService.create(body.email, body.password)
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(+id)
+  }
+
+  @Get()
+  findAllUser(@Query('email') email: string) {
+    return this.usersService.find(email)
+  }
+
+  @Delete('/:id')
+  deleteUser(@Param('id') id: string) {
+    return this.usersService.remove(+id)
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(+id, body)
   }
 }
