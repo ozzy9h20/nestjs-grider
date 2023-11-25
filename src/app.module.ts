c3eb9abc1208458e11094ec8539a16393bee83db
M. Endyarroziqin <endy@millennia-solusi.id>
2023-11-25 21:14:56 +0700

    Section 08 - Chapter 43 ~ Set Up Database Connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UsersModule } from './users/users.module'
@@ -6,7 +7,17 @@ import { ReportsModule } from './reports/reports.module'
 import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, ReportsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: process.env.SQLITE_PATH,
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
