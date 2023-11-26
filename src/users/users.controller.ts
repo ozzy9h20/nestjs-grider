a15bc8a6014dbd1fdc252d069c6dc385cbd65c90
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2023-11-27 05:20:18 +0700

    Section 11 - Chapter 82-87 ~ Custom User Interceptor & Decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   Post,
   Query,
   Session,
+  UseInterceptors,
 } from '@nestjs/common'
 import { UsersService } from './users.service'
 import { AuthService } from './auth.service'
@@ -16,18 +17,27 @@ import { CreateUserDto } from './dtos/create-user.dto'
 import { UpdateUserDto } from './dtos/update-user.dto'
 import { UserDto } from './dtos/user.dto'
 import { Serialize } from 'src/interceptors/serialize.interceptor'
+import { CurrentUser } from './decorators/current-user.decorator'
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor'
+import { User } from './user.entity'
 
 @Controller('auth')
 @Serialize(UserDto)
+@UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
   constructor(
     private usersService: UsersService,
     private authService: AuthService,
   ) {}
 
+  // @Get('/whoami')
+  // whoAmI(@Session() session: any) {
+  //   return this.usersService.findOne(session.userId)
+  // }
+
   @Get('/whoami')
-  whoAmI(@Session() session: any) {
-    return this.usersService.findOne(session.userId)
+  whoAmI(@CurrentUser() user: User) {
+    return user
   }
 
   @Post('/signout')
