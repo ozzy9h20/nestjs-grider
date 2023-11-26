15d1de451ed95acd7488698aa03ab5482db1283f
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2023-11-26 23:36:55 +0700

    Section 11 - Chapter 76 ~ Implement Signin

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,11 @@
 import { randomBytes, scrypt as _scrypt } from 'crypto'
 import { promisify } from 'util'
 import { UsersService } from './users.service'
-import { BadRequestException, Injectable } from '@nestjs/common'
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common'
 
 const scrypt = promisify(_scrypt)
 
@@ -33,5 +37,21 @@ export class AuthService {
     return user
   }
 
-  signin() {}
+  async signin(email: string, password: string) {
+    const [user] = await this.usersService.find(email)
+
+    if (!user) {
+      throw new NotFoundException('user not found')
+    }
+
+    const [salt, storedHash] = user.password.split('.')
+
+    const hash = (await scrypt(password, salt, 32)) as Buffer
+
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('bad password')
+    }
+
+    return user
+  }
 }
