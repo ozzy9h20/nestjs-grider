418498fa1efa45a1d9910a133d9df17a1a49fce2
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2023-11-26 17:10:08 +0700

    Section 10 - Chapter 66-69 ~ Refactor Serialize Decorator

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,16 +1,30 @@
-import { NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common'
+import {
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  UseInterceptors,
+} from '@nestjs/common'
 import { plainToClass } from 'class-transformer'
 import { Observable, map } from 'rxjs'
-import { UserDto } from 'src/users/dtos/user.dto'
+
+interface ClassConstructor {
+  new (...args: any[]): {}
+}
+
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto))
+}
 
 export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: ClassConstructor) {}
+
   intercept(
     context: ExecutionContext,
     next: CallHandler<any>,
   ): Observable<any> | Promise<Observable<any>> {
     return next.handle().pipe(
       map((data: any) => {
-        return plainToClass(UserDto, data, {
+        return plainToClass(this.dto, data, {
           excludeExtraneousValues: true,
         })
       }),
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,14 +8,15 @@ import {
   Patch,
   Post,
   Query,
-  UseInterceptors,
 } from '@nestjs/common'
 import { UsersService } from './users.service'
 import { CreateUserDto } from './dtos/create-user.dto'
 import { UpdateUserDto } from './dtos/update-user.dto'
-import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor'
+import { UserDto } from './dtos/user.dto'
+import { Serialize } from 'src/interceptors/serialize.interceptor'
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
@@ -24,10 +25,8 @@ export class UsersController {
     this.usersService.create(body.email, body.password)
   }
 
-  @UseInterceptors(SerializeInterceptor)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
-    console.log('Handler is running')
     const user = await this.usersService.findOne(+id)
 
     if (!user) {
