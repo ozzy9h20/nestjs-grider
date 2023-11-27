4ccfc0b6976677ce9b44585e954dca126b794728
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2023-11-27 22:41:25 +0700

    Section 14 - Chapter 123 ~ Test WhoAmI Endpoint

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -28,4 +28,22 @@ describe('Authentication System', () => {
         expect(email).toEqual(email)
       })
   })
+
+  it('signup a new user then get the currently logged in user', async () => {
+    const email = 'test@mail.com'
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'test123' })
+      .expect(201)
+
+    const cookie = res.get('Set-Cookie')
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200)
+
+    expect(body.email).toEqual(email)
+  })
 })
