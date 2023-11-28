0e70df59a1e0e31fd418f7fc44bf826c715f53ed
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2023-11-28 09:05:51 +0700

    Section 15 - Chapter 130-137 ~ Associate Reports to Users

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -2,14 +2,19 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common'
 import { CreateReportDto } from './dtos/create-report.dto'
 import { ReportsService } from './reports.service'
 import { AuthGuard } from '../guards/auth.guard'
+import { CurrentUser } from '../users/decorators/current-user.decorator'
+import { User } from '../users/user.entity'
+import { Serialize } from '../interceptors/serialize.interceptor'
+import { ReportDto } from './dtos/report.dto'
 
 @Controller('reports')
+@Serialize(ReportDto)
 export class ReportsController {
   constructor(private reportService: ReportsService) {}
 
   @Post()
   @UseGuards(AuthGuard)
-  createReport(@Body() reportDto: CreateReportDto) {
-    return this.reportService.create(reportDto)
+  createReport(@Body() reportDto: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportService.create(reportDto, user)
   }
 }
