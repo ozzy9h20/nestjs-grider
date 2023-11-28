0cb770d8ec9be54bd1e97a149f74b68c5ae2bc4a
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2023-11-28 22:42:09 +0700

    Section 16 - Chapter 147-148 ~ Transforming Query String Data

---
+++ src/reports/dtos/get-estimate.dto.ts
@@ -0,0 +1,36 @@
+import { Transform } from 'class-transformer'
+import {
+  IsLatitude,
+  IsLongitude,
+  IsNumber,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator'
+export class GetEstimateDto {
+  @IsString()
+  make: string
+
+  @IsString()
+  model: string
+
+  @Transform(({ value }) => +value)
+  @IsNumber()
+  @Min(1930)
+  @Max(2050)
+  year: number
+
+  @Transform(({ value }) => +value)
+  @IsNumber()
+  @Min(0)
+  @Max(1_000_000)
+  mileage: number
+
+  @Transform(({ value }) => parseFloat(value))
+  @IsLongitude()
+  lng: number
+
+  @Transform(({ value }) => parseFloat(value))
+  @IsLatitude()
+  lat: number
+}
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Param, Patch, Post, UseGuards } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common'
 import { CreateReportDto } from './dtos/create-report.dto'
 import { ReportsService } from './reports.service'
 import { AuthGuard } from '../guards/auth.guard'
@@ -8,14 +17,15 @@ import { User } from '../users/user.entity'
 import { Serialize } from '../interceptors/serialize.interceptor'
 import { ReportDto } from './dtos/report.dto'
 import { ApproveReportDto } from './dtos/approve-report.dto'
+import { GetEstimateDto } from './dtos/get-estimate.dto'
 
 @Controller('reports')
-@Serialize(ReportDto)
 export class ReportsController {
   constructor(private reportService: ReportsService) {}
 
   @Post()
   @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
   createReport(@Body() reportDto: CreateReportDto, @CurrentUser() user: User) {
     return this.reportService.create(reportDto, user)
   }
@@ -24,5 +34,10 @@ export class ReportsController {
   @UseGuards(AdminGuard)
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportService.changeApproval(+id, body.approved)
+  }
+
+  @Get('/estimate')
+  getEstimate(@Query() query: GetEstimateDto) {
+    console.log(query)
   }
 }
