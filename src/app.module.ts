92d895fa67c649901011ab974ad68a2b33e08c20
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2023-11-28 23:22:16 +0700

    Section 18 - Chapter 154 ~ Move Cookie Key to .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -43,11 +43,13 @@ const cookieSession = require('cookie-session')
   ],
 })
 export class AppModule {
+  constructor(private configService: ConfigService) {}
+
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(
         cookieSession({
-          keys: ['mySuperSecretKey123'],
+          keys: [this.configService.get('COOKIE_KEY')],
         }),
       )
       .forRoutes('*')
