2973e89081908b7cf5be94e291a08060ade99868
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2023-11-30 18:49:54 +0700

    Section 18 - Chapter 160 ~ Env-Specific Database Config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,10 @@ import { AppService } from './app.service'
 import { UsersModule } from './users/users.module'
 import { ReportsModule } from './reports/reports.module'
 import { ConfigModule, ConfigService } from '@nestjs/config'
-import { User } from './users/user.entity'
-import { Report } from './reports/report.entity'
 import { APP_PIPE } from '@nestjs/core'
 
 const cookieSession = require('cookie-session')
+const dbConfig = require('../ormconfig.js')
 
 @Module({
   imports: [
@@ -17,17 +16,7 @@ const cookieSession = require('cookie-session')
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        return {
-          type: 'sqlite',
-          database: config.get<string>('SQLITE_PATH'),
-          entities: [User, Report],
-          synchronize: true,
-        }
-      },
-    }),
+    TypeOrmModule.forRoot(dbConfig),
     UsersModule,
     ReportsModule,
   ],
